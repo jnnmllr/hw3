e7caa7cd15bbaefa4abe3410b48b5009060c6b00
Janina Müller <janina@Janinas-MBP.lan>
2015-11-17 18:33:39 +0100

    finished hw3

--- statistics.cxx
+++ statistics.cxx
@@ -6,6 +6,27 @@ using namespace std;
 //--------------------------------------------------
 // plenty of lines here
 //--------------------------------------------------
+void fill(double* p, const int N){
+  double b=RAND_MAX;
+for(int i=0; i<N; i++){
+  double a = rand ();
+  p[i] = a/b;
+}
+}
+
+void varmean(double &mean, double &var, double* p, const int N){
+  double sum1;
+  double sum2;
+for(int i=0; i<N; i++){
+  sum1 += p[i];
+}
+mean = sum1/N;
+for(int i=0; i<N; i++){
+sum2 += pow(2,p[i]-mean);
+}
+var = sum2/N;
+}
+
 
 int main(){
    const int N = 100;
@@ -13,6 +34,8 @@ int main(){
    double mean, var;
 
    // Some lines here....
+fill(p, N);
+varmean(mean,var,p,N);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;

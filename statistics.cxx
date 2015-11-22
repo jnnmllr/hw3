8e4bb0522ca45f5257e24cb753a5f17b85af038d
Janina Müller <janina@Janinas-MBP.lan>
2015-11-22 21:04:03 +0100

    corrected hw3

--- statistics.cxx
+++ statistics.cxx
@@ -3,42 +3,46 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
-void fill(double* p, const int N){
-  double b=RAND_MAX;
-for(int i=0; i<N; i++){
-  double a = rand ();
-  p[i] = a/b;
-}
-}
 
-void varmean(double &mean, double &var, double* p, const int N){
-  double sum1;
-  double sum2;
-for(int i=0; i<N; i++){
-  sum1 += p[i];
-}
-mean = sum1/N;
-for(int i=0; i<N; i++){
-sum2 += pow(2,p[i]-mean);
-}
-var = sum2/N;
-}
-
+void fill(double* p, const int N);
+void varmean(double &mean, double &var, double* p, const int N);
 
 int main(){
-   const int N = 100;
+   const int N = 50;
    double p[N];
-   double mean, var;
+   double mean = 0;
+   double var = 0;
 
-   // Some lines here....
 fill(p, N);
 varmean(mean,var,p,N);
 
+// for testing:
+//for(int i=0; i<N; i++){
+//  cout << p[i] << endl;
+//}
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
    return 0;
+}
+
+// function for filling the array
+void fill(double* p, const int N){
+for(int i=0; i<N; i++){
+  double a = double(rand());
+  double b = double(RAND_MAX);
+  p[i] = double(a/b);
+}
+}
+
+// function for calculating the mean value and variance
+void varmean(double &mean, double &var, double* p, const int N){
+for(int i=0; i<N; i++){
+  mean += p[i];
+}
+mean = mean/N;
+for(int i=0; i<N; i++){
+var += pow(p[i]-mean,2);
+}
+var = var/N;
 }
